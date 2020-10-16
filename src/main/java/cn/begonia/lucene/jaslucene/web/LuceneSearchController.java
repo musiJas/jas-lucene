6501128a07e904f22a53c2b3d63e74c7a263369c
begonia <iwrok7083@hotmail.com>
2020-10-16 18:19:52 +0800

    123

--- src/main/java/cn/begonia/lucene/jaslucene/web/LuceneSearchController.java
+++ src/main/java/cn/begonia/lucene/jaslucene/web/LuceneSearchController.java
@@ -2,14 +2,15 @@ package cn.begonia.lucene.jaslucene.web;
 
 import cn.begonia.lucene.jaslucene.common.QueryCondition;
 import cn.begonia.lucene.jaslucene.common.Result;
-import cn.begonia.lucene.jaslucene.service.handler.LuceneReaderService;
 import cn.begonia.lucene.jaslucene.service.search.ISearchService;
-import cn.begonia.lucene.jaslucene.util.DateUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author begonia_chen
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  **/
 
-@RestController
+@Controller
 public class LuceneSearchController {
     private ISearchService  searchService;
     @Autowired
@@ -26,28 +27,61 @@ public class LuceneSearchController {
     }
 
     @RequestMapping("/search")
-    public Result  search(
-                @RequestParam(value = "keyword",required = false) String keyword,
-                @RequestParam(value = "category",required = false) String category,
-                @RequestParam(value = "page",required = false, defaultValue = "0") int  page,
-                @RequestParam(value = "pageSize",required = false,defaultValue = "0") int pageSize
-                ){
-        //luceneReaderService.termQuery("titles",key);
+    public ModelAndView search(
+            @RequestParam(value = "keyword",required = false) String keyword,
+            @RequestParam(value = "category",required = false) String category,
+            @RequestParam(value = "page",required = false, defaultValue = "0") int  page,
+            @RequestParam(value = "pageSize",required = false,defaultValue = "0") int pageSize
+    ){
+        Result  res=null;
+        ModelAndView  modelAndView=new ModelAndView();
         /** 先判断关键词是否为空 如果为空则以默认条件进行检索即时间检索**/
         if(StringUtils.isEmpty(keyword)){
             if(StringUtils.isEmpty(category)){
-                return  searchService.defaultAllCategorySearch(new QueryCondition(page,pageSize));
+                res =searchService.defaultAllCategorySearch(new QueryCondition(page,pageSize));
             }
-            return  searchService.defaultCategorySearch(category);
+            res =searchService.defaultCategorySearch(category);
         }else {
             if(StringUtils.isEmpty(category)){
-                return  searchService.defaultKeywordSearch(keyword);
+                 res=  searchService.defaultKeywordSearch(keyword);
+            }else{
+                res= searchService.search(keyword,category);
             }
-            return searchService.search(keyword,category);
         }
+        modelAndView.setViewName("result");
+        if(StringUtils.isNotEmpty(keyword)){
+            modelAndView.addObject("keyword",keyword);
+        }
+
+        modelAndView.addObject("data",res.getObj());
+        return modelAndView;
     }
 
 
-
+    @RequestMapping("/api/search")
+    @ResponseBody
+    public Result searchApi(
+            @RequestParam(value = "keyword",required = false) String keyword,
+            @RequestParam(value = "category",required = false) String category,
+            @RequestParam(value = "page",required = false, defaultValue = "0") int  page,
+            @RequestParam(value = "pageSize",required = false,defaultValue = "0") int pageSize
+    ){
+        Result  res=null;
+        ModelAndView  modelAndView=new ModelAndView();
+        /** 先判断关键词是否为空 如果为空则以默认条件进行检索即时间检索**/
+        if(StringUtils.isEmpty(keyword)){
+            if(StringUtils.isEmpty(category)){
+                res =searchService.defaultAllCategorySearch(new QueryCondition(page,pageSize));
+            }
+            res =searchService.defaultCategorySearch(category);
+        }else {
+            if(StringUtils.isEmpty(category)){
+                res=  searchService.defaultKeywordSearch(keyword);
+            }else{
+                res= searchService.search(keyword,category);
+            }
+        }
+        return res;
+    }
 
 }
